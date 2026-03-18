50b9f06cd3c1a0b11208ceb64acae075a09c340e
Shivanshu Singh <kshatriyasarkar214@gmail.com>
2026-03-18 20:35:08 +0530

    feat: add Vite configuration with React plugin and API proxy rules for `/api` and `/graphql`.

--- explorers-earth/vite.config.ts
+++ explorers-earth/vite.config.ts
@@ -6,21 +6,23 @@ export default defineConfig({
   plugins: [
     react(),
   ],
-  //  server: {
-  //    proxy: {
-  //      '/api': {
-  //       //  target: 'http://13.126.235.177:1337',
-  //        target: 'http://77.42.95.255:1337',
-  //        changeOrigin: true,
-  //        secure: false,
-  //      },
-  //      '/graphql': {
-  //       //  target: 'http://13.126.235.177:1337',
-  //        target: 'http://77.42.95.255:1337',
-  //        changeOrigin: true,
-  //        secure: false,
-  //      },
-  //    },
-  //  },
+   server: {
+     proxy: {
+       '/api': {
+        //  target: 'http://13.126.235.177:1337',
+        //  target: 'http://77.42.95.255:1337',
+         target: 'https://api.localqr.earth',
+         changeOrigin: true,
+         secure: false,
+       },
+       '/graphql': {
+        //  target: 'http://13.126.235.177:1337',
+        //  target: 'http://77.42.95.255:1337',
+         target: 'https://api.localqr.earth',
+         changeOrigin: true,
+         secure: false,
+       },
+     },
+   },
 });
 
